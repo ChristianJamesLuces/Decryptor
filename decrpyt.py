c6e1dadb6f5778e66eb19e69814ee95316171f68
ChristianJames Luces <christianjames231103@gmail.com>
2023-04-15 20:11:08 +0800

    Pseudocode

---
+++ decrpyt.py
@@ -0,0 +1,17 @@
+# 2: Pseudocode
+# Define variables
+# Display welcome message and its function
+# ask the user for input
+# check each character
+# if *, change it to a
+# if &, change it to e
+# if #, change it to i
+# if +, change it to o
+# if !, change it to u
+# print the output
+# Asking the user if they want to try it again
+# If the user want to try it again 
+# If the user do not want to try it again
+# If the user did not input a valid answer
+# Exit the loop after a valid input is given
+# Go back to the start of the loop
