8170eb0f1301beae96c7d519a7c4dfc34966fe8b
ChristianJames Luces <christianjames231103@gmail.com>
2023-04-15 20:52:14 +0800

    defined variables and welcome message

--- decrpyt.py
+++ decrpyt.py
@@ -1,6 +1,19 @@
 # 2: Pseudocode
+import pyfiglet
+
 # Define variables
+intro = pyfiglet.figlet_format("WELCOME".center(39, "="), font = "digital")
+no_option = ("No", "no", "NO", "n", "N")
+yes_option = ("Yes", "yes","YES","y","Y")
+character_susbsitute = "\t'a' = *\n\t'e' = &\n\t'i' = #\n\t'o' = +\n\t'u' = !\n"
+special_characters = {"*": "a", "&": "e", "#": "i", "+": "o", "!": "u"}
+gratitude = "\033[42m" + "(: Thank you for using this program! :)" + "\033[0m"
+
 # Display welcome message and its function
+print(intro)
+print("\033[45;1m" + "This program will decrypt your encrypted input using the following character substitute:\n" + "\033[0m") # Explains the function of the program
+print("\033[93m" + character_susbsitute + "\033[0m")
+print("-" * 100)
 # ask the user for input
 # check each character
 # if *, change it to a
